291d5f520eee42d59dbbcb7abb6efb2f7f7d946c
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-10-26 10:59:08 +0100

    make drawing function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const board = document.querySelector("#board");
+const board = document.getElementById("board");
 const ctx = board.getContext("2d");
 
-const score = document.querySelector("#score");
+function drawRect(x, y, color) {
+    var size = 20;
+    var padding = 2;
+    ctx.fillStyle = color;
+    ctx.fillRect((size+padding)+x, (size+padding)*y, size, size);
+}
