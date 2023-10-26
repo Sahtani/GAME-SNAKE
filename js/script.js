ec63029d62461710bb3529116754ccf6dfe28a02
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-10-26 11:43:22 +0100

    draw things in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 const board = document.getElementById("board");
 const ctx = board.getContext("2d");
 
-function drawRect(x, y, color) {
-    var size = 20;
-    var padding = 2;
-    ctx.fillStyle = color;
-    ctx.fillRect((size+padding)+x, (size+padding)*y, size, size);
-}
+var rows = 20;
+var cols = 20;
+var blockSize = 25;
+
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+board.height = rows * blockSize;
+board.width = cols * blockSize;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, board.width, board.height);
+
+ctx.fillStyle = "lime";
+ctx.fillRect(snakeX, snakeY, blockSize, blockSize);
